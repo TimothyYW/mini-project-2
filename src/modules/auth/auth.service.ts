4ab43eba09fd6b026b7180a6944f58995cd49cdd
Timothy <timothy.yeremia.wewengkang@gmail.com>
2025-12-17 19:24:17 +0700

    demo transaction and expired transaction

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,13 @@
 import { sign } from "jsonwebtoken";
+import { BASE_URL_FE } from "../../config/env";
 import { ApiError } from "../../utils/api-error";
 import { comparePassword, hashPassword } from "../../utils/password";
+import { MailService } from "../mail/mail.service";
 import { PrismaService } from "../prisma/prisma.service";
+import { ForgotPasswordDTO } from "./dto/forgot-password.dto";
 import { LoginDTO } from "./dto/login.dto";
 import { RegisterDTO } from "./dto/register.dto";
-import { MailService } from "../mail/mail.service";
-import { ForgotPasswordDTO } from "./dto/forgot-password.dto";
 import { ResetPasswordDTO } from "./dto/reset-password.dto";
-import { BASE_URL_FE } from "../../config/env";
 
 export class AuthService {
   prisma: PrismaService;
@@ -75,7 +75,7 @@ export class AuthService {
       "Forgot Password",
       "forgot-password",
       {
-        resetUrl: `${BASE_URL_FE}/reset-password?token=${accessToken}`,
+        resetUrl: `${BASE_URL_FE}/reset-password/${accessToken}`,
       }
     );
 
